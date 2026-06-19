3c9570d4cc4aa2cb8163bb200f4c1fdab398c0f0
hansenjonatann <hansenjonatann@gmail.com>
2026-06-19 10:08:00 +0700

    feat: update .env file and db.go

--- backend/config/db.go
+++ backend/config/db.go
@@ -7,9 +7,10 @@ import (
 	"citramascoweb-backend/internal/modules/rooms/category"
 	"citramascoweb-backend/internal/modules/rooms/reservation"
 	"citramascoweb-backend/internal/modules/rooms/types"
+	"fmt"
 	"log"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -17,13 +18,20 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() *gorm.DB {
-	dsn := Config("DB_URL")
+	host := Config("DB_HOST")
+	user := Config("DB_USER")
+	password := Config("DB_PASSWORD")
+	port := Config("DB_PORT")
+	dbname := Config("DB_NAME")
 
+	// Rangkai menjadi format DSN PostgreSQL yang resmi
+	dsn := fmt.Sprintf("host=%s user=%s password=%s port=%s dbname=%s sslmode=require",
+		host, user, password, port, dbname)
 	if dsn == "" {
 		log.Fatalf("Database connection failed: DB_URL environment variable is missing or empty")
 	}
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 
--- backend/go.mod
+++ backend/go.mod
@@ -26,6 +26,10 @@ require (
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -43,7 +47,9 @@ require (
 	golang.org/x/arch v0.23.0 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/net v0.51.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.35.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
+	gorm.io/driver/postgres v1.6.0 // indirect
 )
